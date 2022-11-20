55a7b0a611b3a3babc3ab6353d7895f06c9f9943
FeiOuSama <380302391@qq.com>
2022-11-20 11:15:22 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,19 @@
+﻿// Hazel
+
+#pragma once
+
+#include "Core.h"
+
+namespace Hazel
+{
+	class HAZEL_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+		void Run(); 
+	};
+
+	// To be define clients
+	Application* CreateApplication();
+}
